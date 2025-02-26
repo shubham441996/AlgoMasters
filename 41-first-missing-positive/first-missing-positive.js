ab9d0e900ffa1e855fb3f5f09a92056a98ae2c13
Shubham Paliwal <shubhpaliwal98@gmail.com>
2025-02-26 18:31:00 +0530

    Time: 11 ms (59.10%) | Memory: 72.8 MB (11.51%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    nums = nums.filter(n => n > 0);
+
+    for (let n of nums) {
+      const idx = Math.abs(n) - 1;
+      if (idx < nums.length && nums[idx] > 0) {
+        nums[idx] *= -1;
+      }
+    }
+    
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > 0) {
+        return i + 1;
+      }
+    }
+    
+    return nums.length + 1;
+
+};
